1ae2374506b79c698174291c4bac2f05a41307d8
feme12 <96890914+feme12@users.noreply.github.com>
2024-05-25 08:52:11 -0500

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,25 +1,2 @@
 local test = "value testing thing"
-
-local bool = true
-
-if 1+1 == 2 then
-    print("Check 1")
-end
-
-if 1+2 == 3 then
-    print("Check 2")
-end
-
-if 2+2 == 4 then
-    print("Check 3")
-end
-
-if bool then
-    print("Check 4")
-end
-
-if bool == true then
-    print("Check 5")
-end
-
 print(test)
